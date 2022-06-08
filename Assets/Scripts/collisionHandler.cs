e126b84e1e1bc20ff01ea951e78eff8d6168bf33
NogaBartal <75697299+NogaBartal@users.noreply.github.com>
2022-06-08 17:53:58 +0300

    adding new level

--- Assets/Scripts/collisionHandler.cs
+++ Assets/Scripts/collisionHandler.cs
@@ -5,13 +5,13 @@ public class collisionHandler : MonoBehaviour
 {
     void OnCollisionEnter(Collision other) 
     {
-       switch(other.gameObject.tag)
-       {
+        switch(other.gameObject.tag)
+        {
             case "Friendly":
                 Debug.Log("this thing is friendly");
                 break;
             case "Finish":
-                Debug.Log("Mission Acoplish");
+                ReloadNextLevel();
                 break;
             default:
                 ReloadLevel();
@@ -22,7 +22,18 @@ public class collisionHandler : MonoBehaviour
 
     void ReloadLevel()
     {
-        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(sceneIndex);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void ReloadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
